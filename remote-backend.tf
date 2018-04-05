1543a92687e17ec29041571c3177ad73e5537e32
Adron Hall <adronhall@gmail.com>
2018-04-05 13:56:30 -0700

    First commit.

---
+++ remote-backend.tf
@@ -0,0 +1,10 @@
+ terraform {
+   required_version = "~> 0.10"
+
+   backend "s3" {
+     encrypt = true
+     bucket = "terraform-backend-dalles"
+     key = "ourdatastore/terraform.tfstate"
+     region = "us-west-2"
+   }
+ }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_name" {
+  description = "The name of the VPC network."
+}
+
+variable "s3_terraform_bucket" {
+
+}
+
+variable "environment" {}
+variable "region" {}
